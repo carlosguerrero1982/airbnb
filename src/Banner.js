f8e81870e34bcdc87ff56a7e1ccdf07b42565aef
= <=>
2020-10-13 16:24:20 +0200

    131020:1624

--- src/Banner.js
+++ src/Banner.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './Banner.css';
 import {Button} from '@material-ui/core';
+import Search from './Search';
 
 function Banner() {
+
+    const [showSearch, setshowSearch] = useState(false);
+
+
     return (
         <div className="banner">
+
+            <div className="banner_search"> 
+
+             {
+                 showSearch && < Search />
+             }
+
+                <Button onClick={()=>setshowSearch(! showSearch)} className="banner_searchButton" variant="outlined">
+                   
+                   {showSearch?"Hide":"Search Dates"}
+                    
+                    
+                </Button>
+
+            </div> 
 
             <div className="banner_info">
 
